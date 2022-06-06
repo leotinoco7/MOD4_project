af9b997cb2fd826ff8e4c91391a95f1219e60e15
leotinoco7 <leofptinoco7@gmail.com>
2022-06-06 16:43:14 -0300

    feat(bcrypt) add encryptatioN fix(entity) match entity names fix(schema) match names

--- src/user/dto/create-user.dto.ts
+++ src/user/dto/create-user.dto.ts
@@ -1,23 +1,24 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { IsNotEmpty, IsString, IsUrl, Length } from 'class-validator';
+import { IsEmail, IsInt, IsNotEmpty, IsString, Length } from 'class-validator';
 
 export class CreateUserDto {
-  @Length(3, 10)
+  @IsEmail()
   @ApiProperty({
-    description: 'Nome do usuário. Deve conter de 3 a 10 letras',
-    example: 'João',
+    description: 'Email do usuário',
+    example: 'leonardo@gmail.com',
+  })
+  @IsNotEmpty()
+  @IsString()
+  email: string;
+
+  @Length(8, 24)
+  @ApiProperty({
+    description: 'Nome do usuário. Entre 8 e 24 caracteres',
+    example: 'Leonardo Tinoco',
   })
   @IsNotEmpty()
   @IsString()
   name: string;
-
-  @ApiProperty({
-    description: 'Nickname',
-    example: 'joao123',
-  })
-  @IsNotEmpty()
-  @IsString()
-  nickname: string;
 
   @Length(8, 16)
   @ApiProperty({
@@ -28,11 +29,18 @@ export class CreateUserDto {
   @IsString()
   password: string;
 
+  @Length(11)
   @ApiProperty({
-    description: 'Avatar',
-    example: 'url da imagem',
+    description: 'CPF (somente os dígitos, sem hifen etc)',
+    example: '12312312300',
   })
-  @IsUrl()
-  @IsString()
-  image: string;
+  @IsNotEmpty()
+  @IsInt()
+  cpf: number;
+
+  @ApiProperty({
+    description: 'confirm pw',
+    example: '??',
+  })
+  confirmPassword: string;
 }
